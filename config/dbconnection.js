68548482859239385791c7dcab8bbc2aadcca5f7
Edilson Chagas <portasoftsistemas@gmail.com>
2018-11-14 15:24:13 -0200

    Troca de imagem

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -7,7 +7,7 @@ var database = 'dados2182n';
 // instanciar objeto de acesso ao banco de dados
 var client = mysql.createConnection({
     user: 'root',
-    password: 'INFORME AQUI A SENHA DO MYSQL',
+    password: '#INFORME AQUI A SENHA DO MYSQL',
     host: 'localhost',
     port: 3306
 });
